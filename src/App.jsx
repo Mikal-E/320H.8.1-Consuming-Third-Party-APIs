6213c8d3d122216d3c61c4e48360170caa1df0ec
Mikal-E <melamin101@gmail.com>
2026-07-23 14:43:24 -0400

    Built out function App() in App.jsx. Still need to complete the retirn.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from "react";
+import { getAllStarships } from "./services/sw-api";
+import StarshipCard from "./components.StarshipCard.jsx";
+
+/* Requirements/Hints: Hold the starship objects in state with the useState hook. Use the useEffect hook. Be sure to update state with the setter function.
+.map() over each starship object in state to transform them into a <StarshipCard /> component. 
+
+Requirements: 5. Obtain all of the starships from the API and render a card for each starship within <App>.*/
+
 function App() {
 
+    const [starships, setStarships] = useState([]);
+
+    useEffect (() => {
+
+        async function fetchStarships() {
+
+            try {
+
+                const data = await getAllStarships();
+                setStarships(dtat);
+                
+            } catch (error) {
+
+                console.error(error);
+                
+            }
+
+        }
+
+        fetchStarships();
+
+    }, []);
+    
     return (
 
         <div>
