80466dd6d6075e20fed68437b27fc5d1ec4f5dfb
Mikal-E <melamin101@gmail.com>
2026-07-23 14:59:40 -0400

    Created the StarshhipCard.jsx component.

---
+++ src/components/StarshipCard.jsx
@@ -0,0 +1,19 @@
+/* Requirements/Hints: Create a StarShipCard component (that App.jsx will import).
+
+Requirements: 6. Cards should contain the text of the starship's name. */
+
+function StarshipCard({ starship }) {
+
+    return (
+
+        <div>
+
+            <p>{starship.name}</p>
+
+        </div>
+
+    )
+
+}
+
+export default StarshipCard;
